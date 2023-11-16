02294e2d5dd09ba9799fdb944644651aebd4efc3
YAAKOUBI Khaoula <khaoula.yaakoubi@forvia.com>
2023-11-16 22:16:27 +0100

    khaoula updated vendeur class

--- prosit4/Vendeur.java
+++ prosit4/Vendeur.java
@@ -1,14 +1,44 @@
 package prosit4;
 
+/**
+ * Represents a salesperson who is an employee.
+ */
 class Vendeur extends Employe {
-    protected double tauxDeVente;
+    private double tauxDeVente;
 
+    // Constants if applicable
+    private static final double DEFAULT_TAUX_DE_VENTE = 0.0;
+
+    /**
+     * Constructs a salesperson with the specified attributes.
+     *
+     * @param id           the ID of the salesperson.
+     * @param nom          the name of the salesperson.
+     * @param adresse      the address of the salesperson.
+     * @param nbrHeures    the number of hours worked by the salesperson.
+     * @param tauxDeVente  the sales rate of the salesperson.
+     */
     public Vendeur(int id, String nom, String adresse, int nbrHeures, double tauxDeVente) {
         super(id, nom, adresse, nbrHeures);
         this.tauxDeVente = tauxDeVente;
     }
 
+    /**
+     * Gets the sales rate of the salesperson.
+     *
+     * @return the sales rate of the salesperson.
+     */
     public double getTauxDeVente() {
         return tauxDeVente;
+    }
+
+    /**
+     * Sets the sales rate of the salesperson.
+     *
+     * @param tauxDeVente the new sales rate to set.
+     */
+    public void setTauxDeVente(double tauxDeVente) {
+        // Add validation if necessary
+        this.tauxDeVente = tauxDeVente;
     }
 }
