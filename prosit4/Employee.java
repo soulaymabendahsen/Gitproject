f662fc179e59669f2a2fe98f3f92ff70538c0651
YAAKOUBI Khaoula <khaoula.yaakoubi@forvia.com>
2023-11-16 23:03:43 +0100

    employee updated again by yaakoubi

--- prosit4/Employee.java
+++ prosit4/Employee.java
@@ -5,16 +5,9 @@ class Employe {
     protected String nom;
     protected String adresse;
     protected int nbr_heures;
-    private static final int DEFAULT_NBR_HEURES = 0;
 
-    /**
-     * Constructs an employee with the specified attributes.
-     *
-     * @param id        the ID of the employee.
-     * @param nom       the name of the employee.
-     * @param adresse   the address of the employee.
-     * @param nbr_heures the number of hours worked by the employee.
-     */
+
+    
     public Employe(int id, String nom, String adresse, int nbr_heures) {
         this.id = id;
         this.nom = nom;
